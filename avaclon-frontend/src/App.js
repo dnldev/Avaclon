9b5e3e03302fec0b062d4288332c8e1a07e674ac
unknown <daniel.ungersboeck@gmail.com>
2018-05-19 16:04:05 +0200

    implement basic material-ui app layout

--- avaclon-frontend/src/App.js
+++ avaclon-frontend/src/App.js
@@ -1,19 +1,46 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+
+import { createMuiTheme } from "@material-ui/core/styles";
+import { MuiThemeProvider } from "@material-ui/core";
+
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      light: "#fff263",
+      main: "#fbc02d",
+      dark: "#c49000",
+      contrastText: "#000",
+    },
+    secondary: {
+      light: "#ffffff",
+      main: "#ced7db",
+      dark: "#9da6a9",
+      contrastText: "#000",
+    }
+  },
+});
 
 class App extends Component {
+  componentDidMount() {
+    document.title = "Avaclon";
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <MuiThemeProvider theme={theme}>
+        <AppBar position="static" color="primary">
+          <Toolbar>
+            <Typography variant="title" color="inherit">
+              Avaclon
+            </Typography>
+          </Toolbar>
+        </AppBar>
+        {/* other components */}
+      </MuiThemeProvider>
     );
   }
 }
