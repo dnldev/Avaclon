e9a63c01090a440a9a76773efbbf3da252539fe4
unknown <daniel.ungersboeck@gmail.com>
2018-05-19 19:09:51 +0200

    use css instead of html properties for root element

--- avaclon-frontend/src/components/Mission.jsx
+++ avaclon-frontend/src/components/Mission.jsx
@@ -4,7 +4,10 @@ import PropTypes from "prop-types";
 import { withStyles } from "@material-ui/core/styles";
 
 const styles = theme => ({
-  root: {}
+  root: {
+    height: 100,
+    width: 100
+  }
 });
 
 class Mission extends Component {
@@ -12,8 +15,8 @@ class Mission extends Component {
     const { classes } = this.props;
 
     return (
-      <svg height="100" width="100">
-        <circle cx="50" cy="50" r="40" stroke={ this.props.missionIndex == this.props.currentMission ? "red" : "black" } strokeWidth="3" 
+      <svg className={classes.root}>
+        <circle cx="50" cy="50" r="40" stroke={ this.props.missionIndex === this.props.currentMission ? "red" : "black" } strokeWidth="3" 
                 fill={this.props.wonBy === undefined ? "transparent" : this.props.wonBy === "evil" ? "red" : "blue"}/>
         <text fontSize="2em" x="50%" y="50%" textAnchor="middle" dy=".33em">
           {this.props.playersOnMission}
