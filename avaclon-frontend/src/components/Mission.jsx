f4170ad28c4c49fadca904f23a4fc4cc12cada1f
unknown <daniel.ungersboeck@gmail.com>
2018-05-19 19:05:24 +0200

    mission class with different styles

---
+++ avaclon-frontend/src/components/Mission.jsx
@@ -0,0 +1,30 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+
+import { withStyles } from "@material-ui/core/styles";
+
+const styles = theme => ({
+  root: {}
+});
+
+class Mission extends Component {
+  render() {
+    const { classes } = this.props;
+
+    return (
+      <svg height="100" width="100">
+        <circle cx="50" cy="50" r="40" stroke={ this.props.missionIndex == this.props.currentMission ? "red" : "black" } strokeWidth="3" 
+                fill={this.props.wonBy === undefined ? "transparent" : this.props.wonBy === "evil" ? "red" : "blue"}/>
+        <text fontSize="2em" x="50%" y="50%" textAnchor="middle" dy=".33em">
+          {this.props.playersOnMission}
+        </text>
+      </svg>
+    );
+  }
+}
+
+Mission.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(Mission);
