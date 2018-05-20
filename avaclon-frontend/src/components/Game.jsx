4b3e9223cb0eaf0e1aa596f247c5845ddbfa110d
unknown <daniel.ungersboeck@gmail.com>
2018-05-20 22:54:15 +0200

    implement localization and switchLanguage button

--- avaclon-frontend/src/components/Game.jsx
+++ avaclon-frontend/src/components/Game.jsx
@@ -8,18 +8,22 @@ import Button from "@material-ui/core/Button";
 
 import Board from "./Board";
 
+import strings from "../localization/game-locale";
+
 import GameContext from "./game-context";
 
 const styles = theme => ({
   root: {
     margin: theme.spacing.unit
   },
-  newGameButton: {
-    marginTop: theme.spacing.unit * 2
+  button: {
+    margin: theme.spacing.unit * 2
   }
 });
 
 class Game extends Component {
+  currentLanguage = "en";
+
   startingValues = {
     currentMission: 0,
     gameEnded: false,
@@ -37,8 +41,10 @@ class Game extends Component {
 
     this.getClonedStartingValues = this.getClonedStartingValues.bind(this);
     this.resetGame = this.resetGame.bind(this);
+    this.switchLanguage = this.switchLanguage.bind(this);
   }
 
+  
   currentMissionWon(winner) {
     this.setState(prevState => {
       prevState.wonMissions.push(winner);
@@ -49,29 +55,38 @@ class Game extends Component {
       }
     })
   }
-
+  
   getClonedStartingValues() {
     return JSON.parse(JSON.stringify(this.startingValues)); // clone object
   }
-
+  
   // TODO: socket.io back-end check
   hasGameEnded(wonMissions) {
     return ((wonMissions.filter(el => el === "evil")).length === 3
-            || (wonMissions.filter(el => el === "good")).length === 3);
+    || (wonMissions.filter(el => el === "good")).length === 3);
   }
-
+  
   resetGame() {
     this.setState(this.getClonedStartingValues());
+  }
+  
+  switchLanguage() {
+    this.currentLanguage = this.currentLanguage === "en" ? "de" : "en";
+    strings.setLanguage(this.currentLanguage);
+    this.setState({});
   }
 
   render() {
     const { classes } = this.props;
-
+    
     return (
       <div className={classes.root}>
         <GameContext.Provider value={this.state}>
-          <Button className={classes.newGameButton} variant="raised" color="primary" onClick={() => this.resetGame()}>
-            New Game
+          <Button className={classes.button} variant="raised" color="primary" onClick={() => this.resetGame()}>
+            {strings.newGame}
+          </Button>
+          <Button className={classes.button} variant="raised" color="primary" onClick={() => this.switchLanguage()}>
+            {strings.switchLanguage}
           </Button>
           <Board />
         </GameContext.Provider>
