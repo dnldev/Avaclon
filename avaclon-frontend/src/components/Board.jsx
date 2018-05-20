217e4ea5fe535265e3206cd78960656fb7a2da54
unknown <daniel.ungersboeck@gmail.com>
2018-05-20 13:47:15 +0200

    remove internal state management use context api instead (with central state)

--- avaclon-frontend/src/components/Board.jsx
+++ avaclon-frontend/src/components/Board.jsx
@@ -9,6 +9,8 @@ import Typography from "@material-ui/core/Typography";
 
 import Mission from "./Mission";
 
+import GameContext from "./game-context";
+
 const styles = theme => ({
   root: theme.mixins.gutters({
     paddingTop: 16,
@@ -21,46 +23,33 @@ class Board extends Component {
   missionsPlayerDict = {5: [2, 3, 2, 3, 3], 6: [2, 3, 4, 3, 4], 7: [2, 3, 3, 4, 4], 8: [3, 4, 4, 5, 5], 
                         9: [3, 4, 4, 5, 5], 10: [3, 4, 4, 5, 5]};
 
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      currentMission: 0,
-      voteMarker: 0,
-      missionsWon: []
-    };
-  }
-
-  missionWon(winner) {
-    this.setState(prevState => {
-      return { 
-        currentMission: prevState.currentMission + 1,
-        missionWon: prevState.missionsWon.push(winner)
-      }
-    });
-  }
-
   render() {
     const { classes } = this.props;
 
     return (
-      <Paper className={classes.root} elevation={10}>
-        <Typography component="p">
-            Current Mission: {this.state.currentMission}
-          </Typography>
+        <GameContext.Consumer>
+          {(context) => {
+            return (
+              <Paper className={classes.root} elevation={10}>
+                <Typography component="p">
+                  Current Mission: {context.currentMission}
+                </Typography>
 
-        {this.missionsPlayerDict[this.props.playerCount].map((missionPlayers, i) => {
-          return (<Mission key={i} currentMission={this.state.currentMission} missionIndex={i}
-                  playersOnMission={missionPlayers} wonBy={this.state.missionsWon[i]}/>)
-        })}
-        <br/>
-        <Button size="small" className={classes.button} onClick={() => this.missionWon("evil")}>
-          redWin
-        </Button>
-        <Button size="small" className={classes.button} onClick={() => this.missionWon("good")}>
-          blueWin
-        </Button>
-      </Paper>
+                {this.missionsPlayerDict[context.playerCount].map((missionPlayers, i) => {
+                  return (<Mission key={i} currentMission={context.currentMission} missionIndex={i}
+                          playersOnMission={missionPlayers} wonBy={context.missionsWon[i]}/>)
+                })}
+                <br/>
+                <Button size="small" className={classes.button} onClick={() => context.currentMissionWon("evil")}>
+                  redWin
+                </Button>
+                <Button size="small" className={classes.button} onClick={() => context.currentMissionWon("good")}>
+                  blueWin
+                </Button>
+              </Paper>
+            )
+          }}
+        </GameContext.Consumer>
     );
   }
 }
