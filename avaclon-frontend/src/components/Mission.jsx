b937009731fedb56c06d3ac3ef272d4df1c331c1
unknown <daniel.ungersboeck@gmail.com>
2018-05-20 16:13:18 +0200

    fix styling and refactor class logic

--- avaclon-frontend/src/components/Mission.jsx
+++ avaclon-frontend/src/components/Mission.jsx
@@ -7,18 +7,53 @@ const styles = theme => ({
   root: {
     height: 100,
     width: 100
+  },
+  circle: {
+    stroke: theme.palette.secondary.dark,
+    cx: 50,
+    cy: 50,
+    r: 40,
+    strokeWidth: 3
+  },
+  currentMissionCircle: {
+    stroke: theme.palette.primary.dark
+  },
+  goodWinCircleFill: {
+    fill: theme.palette.teams.good
+  },
+  evilWinCircleFill: {
+    fill: theme.palette.teams.evil
+  },
+  neutralCircleFill: {
+    fill: "transparent"
   }
 });
 
 class Mission extends Component {
+  isCurrentMission() {
+    return this.props.missionIndex === this.props.currentMission;
+  }
+
+  getCircleClasses(classes) {
+    return [
+      classes.circle,
+      this.isCurrentMission() ? classes.currentMissionCircle : '',
+      this.getMissionStatusClass(classes)
+    ].join(" ");
+  }
+
+  getMissionStatusClass(classes) {
+    return this.props.wonBy === undefined ? classes.neutralCircleFill 
+           : this.props.wonBy === "evil" ? classes.evilWinCircleFill : classes.goodWinCircleFill
+  }
+
   render() {
     const { classes } = this.props;
 
     return (
       <svg className={classes.root}>
-        <circle cx="50" cy="50" r="40" stroke={ this.props.missionIndex === this.props.currentMission ? "red" : "black" } strokeWidth="3" 
-                fill={this.props.wonBy === undefined ? "transparent" : this.props.wonBy === "evil" ? "red" : "blue"}/>
-        <text fontSize="2em" x="50%" y="50%" textAnchor="middle" dy=".33em">
+        <circle className={this.getCircleClasses(classes)} />
+        <text fontSize="2em" x="50%" y="50%" text-anchor="middle" dy=".33em">
           {this.props.playersOnMission}
         </text>
       </svg>
