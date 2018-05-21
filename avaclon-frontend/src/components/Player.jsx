ea24c3236bd7249c58e5372ea3963066150c55b9
JoshHash <josh.hadu@gmai.com>
2018-05-21 10:22:12 +0200

    extend player class

--- avaclon-frontend/src/components/Player.jsx
+++ avaclon-frontend/src/components/Player.jsx
@@ -8,8 +8,8 @@ import Button from "@material-ui/core/Button";
 const styles = theme => ({
   root: {
     bottom: theme.spacing.unit * 2,
-    position: "fixed",
     left: "35%",
+    position: "fixed",
     width: "30%"
   },
   playerName: {
@@ -24,27 +24,65 @@ const styles = theme => ({
   },
   playerData: {},
   cardImage: {
-    height: "120%",
-  }
+    height: "120%"
+  },
+  evilPlayer: {
+    textDecorationColor: theme.palette.teams.evil,
+    textDecorationLine: "underline",
+    textDecorationStyle: "solid",
+  },
+  goodPlayer: {
+    textDecorationColor: theme.palette.teams.good,
+    textDecorationLine: "underline",
+    textDecorationStyle: "solid",
+  },
 });
 
 class Player extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.sendVote = this.sendVote.bind(this);
+    this.getPlayerNameClass = this.getPlayerNameClass.bind(this);
+  }
+
+  sendVote(vote) {
+    // TODO: emit event
+    // TODO: visual presentation for successfull vote
+  }
+
+  getPlayerNameClass(classes) {
+    return this.props.player.role.affiliation === "evil" ? classes.evilPlayer : classes.goodPlayer; 
+  }
+
   render() {
     const { classes } = this.props;
     const { role } = this.props.player;
+    const headerClasses = [classes.playerName, this.getPlayerNameClass(classes)].join(" ");
 
     return (
       <Paper className={classes.root}>
         <Grid container className={classes.playerControls}>
           <Grid item xs={9} className={classes.playerData}>
-            <Typography className={classes.playerName} component="h3">
+            <Typography className={headerClasses} variant="headline" component="h3">
               {this.props.player.name}
             </Typography>
             <Divider />
             <div className={classes.playerControls}>
               <div className="buttonArea">
-                <Button className={classes.voteButton}>Approve</Button>
-                <Button className={classes.voteButton}>Reject</Button>
+                <Button
+                  className={classes.voteButton}
+                  onClick={this.sendVote("Approve")}
+                >
+                  Approve
+                </Button>
+                <Button
+                  className={classes.voteButton}
+                  onClick={this.sendVote("Reject")}
+                >
+                  Reject
+                </Button>
               </div>
             </div>
           </Grid>
@@ -59,7 +97,7 @@ class Player extends Component {
 
 Player.propTypes = {
   classes: PropTypes.object.isRequired,
-  player: PropTypes.object.isRequired,
+  player: PropTypes.object.isRequired
 };
 
 export default withStyles(styles)(Player);
