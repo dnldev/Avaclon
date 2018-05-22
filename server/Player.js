e0546a2a4f015b4e47d37386cc3fa0610569f13c
JoshHash <josh.hadu@gmai.com>
2018-05-22 17:25:46 +0200

    give player id field

--- server/Player.js
+++ server/Player.js
@@ -1,7 +1,8 @@
 const Role = require("./Role").Role;
 
 class Player {
-    constructor(playerName, role) {
+    constructor(playerName, role, id) {
+        this.id = id;
         this.name = playerName;
         this.role = new Role(role);
     }
