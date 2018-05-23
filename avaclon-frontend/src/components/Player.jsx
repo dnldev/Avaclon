8a7cd58a0e8e751cacca7ca62519aca113746ee5
JoshHash <josh.hadu@gmai.com>
2018-05-23 09:12:27 +0200

    add required prop playerInfo to player

--- avaclon-frontend/src/components/Player.jsx
+++ avaclon-frontend/src/components/Player.jsx
@@ -2,21 +2,32 @@ import React, { Component } from "react"
 import PropTypes from "prop-types";
 
 import { withStyles } from "@material-ui/core/styles";
-import { Chip, Avatar } from "@material-ui/core";
+import { Chip, Avatar, Icon } from "@material-ui/core";
 
 const styles = theme => ({
     root: {
         margin: theme.spacing.unit,
     },
     tokenArea: {
-        margin: theme.spacing.unit,
+        height: "3%",
+        marginBottom: theme.spacing.unit * -0.5,
+        marginLeft: theme.spacing.unit,
     },
 });
 
 class Player extends Component {
 
-    showInfo() {
+    constructor(props) {
+        super(props);
 
+        this.state = {
+            isLeader: true,
+            inTeam: false
+        }
+    }
+
+    showInfo(infoObject) {
+        console.log(infoObject);
     }
 
     render() {
@@ -24,14 +35,15 @@ class Player extends Component {
 
         return (
             <div className={classes.root}>
-                <Chip
-                    avatar={<Avatar>PL</Avatar>}
-                    label="Player"
-                    onClick={this.showInfo}
-                />
                 <div className={classes.tokenArea}>
-                    
+                    {this.state.inTeam && <Icon>star_border</Icon>}
+                    {this.state.isLeader && <Icon>group</Icon>}
                 </div>
+                <Chip
+                    avatar={<Avatar>{this.props.playerInfo.name.substring(0, 2)}</Avatar>}
+                    label={this.props.playerInfo.name}
+                    onClick={() => this.showInfo(this.props.playerInfo.known)}
+                />
             </div>
         );
     }
@@ -39,6 +51,7 @@ class Player extends Component {
 
 Player.propTypes = {
     classes: PropTypes.object.isRequired,
+    playerInfo: PropTypes.object.isRequired
 };
 
 export default withStyles(styles)(Player)
