7a1dc7c02368e6feebaa008d5d9baad0924fa4d2
unknown <daniel.ungersboeck@gmail.com>
2018-05-25 10:07:28 +0200

    remove temporary game manipulation buttons from board + refactoring

--- avaclon-frontend/src/components/Board.jsx
+++ avaclon-frontend/src/components/Board.jsx
@@ -4,63 +4,53 @@ import PropTypes from "prop-types";
 import { withStyles } from "@material-ui/core/styles";
 
 import Paper from "@material-ui/core/Paper";
-import Button from "@material-ui/core/Button";
 import Typography from "@material-ui/core/Typography";
 
 import Mission from "./Mission";
+
+import strings from "../localization/game-locale";
+
+import GameContext from "./game-context";
 
 const styles = theme => ({
   root: theme.mixins.gutters({
     paddingTop: 16,
     paddingBottom: 16,
     marginTop: theme.spacing.unit * 3,
-  })
+  }),
+  headline: {
+    marginBottom: theme.spacing.unit,
+  }
 });
 
 class Board extends Component {
-  missionsPlayerDict = {5: [2, 3, 2, 3, 3], 6: [2, 3, 4, 3, 4], 7: [2, 3, 3, 4, 4], 8: [3, 4, 4, 5, 5], 
-                        9: [3, 4, 4, 5, 5], 10: [3, 4, 4, 5, 5]};
-
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      currentMission: 0,
-      voteMarker: 0,
-      missionsWon: []
-    };
-  }
-
-  missionWon(winner) {
-    this.setState(prevState => {
-      return { 
-        currentMission: prevState.currentMission + 1,
-        missionWon: prevState.missionsWon.push(winner)
-      }
-    });
-  }
+  missionPlayersForPlayerCount = {
+    5: [2, 3, 2, 3, 3], 6: [2, 3, 4, 3, 4], 7: [2, 3, 3, 4, 4], 
+    8: [3, 4, 4, 5, 5], 9: [3, 4, 4, 5, 5], 10: [3, 4, 4, 5, 5]
+  };
 
   render() {
     const { classes } = this.props;
 
     return (
-      <Paper className={classes.root} elevation={10}>
-        <Typography component="p">
-            Current Mission: {this.state.currentMission}
-          </Typography>
+        <GameContext.Consumer>
+          {(context) => {
+            return (
+              <Paper className={classes.root} elevation={5}>
+                <Typography className={classes.headline} variant="headline" component="h3">
+                  {strings.currentMission}: {context.currentMission + 1}
+                </Typography>
 
-        {this.missionsPlayerDict[this.props.playerCount].map((missionPlayers, i) => {
-          return (<Mission key={i} currentMission={this.state.currentMission} missionIndex={i}
-                  playersOnMission={missionPlayers} wonBy={this.state.missionsWon[i]}/>)
-        })}
-        <br/>
-        <Button size="small" className={classes.button} onClick={() => this.missionWon("evil")}>
-          redWin
-        </Button>
-        <Button size="small" className={classes.button} onClick={() => this.missionWon("good")}>
-          blueWin
-        </Button>
-      </Paper>
+                {this.missionPlayersForPlayerCount[context.playerCount]
+                    .map((missionPlayers, i) => {
+                      return (<Mission key={i} missionIndex={i} currentMission={context.currentMission}
+                              playersOnMission={missionPlayers} wonBy={context.wonMissions[i]}/>)
+                    })
+                }
+              </Paper>
+            )
+          }}
+        </GameContext.Consumer>
     );
   }
 }
