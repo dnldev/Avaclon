0f66991d7ee8126292da04a96813d1c96e2d35fa
JoshHash <josh.hadu@gmai.com>
2018-05-28 11:28:36 +0200

    add customizable button

--- avaclon-frontend/src/components/AutoCollapsing.jsx
+++ avaclon-frontend/src/components/AutoCollapsing.jsx
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
-import { Hidden } from "@material-ui/core";
+import Hidden from "@material-ui/core/Hidden";
+import Button from "@material-ui/core/Button";
+import Icon from "@material-ui/core/Icon";
 
 const sizes = ['xs', 'sm', 'md', 'lg', 'xl'];
 
@@ -10,13 +12,18 @@ class AutoCollapsing extends Component {
     const breakPosition = sizes.indexOf(this.props.breakAt) + 1;
     const collapsedSizes = sizes.slice(0, breakPosition);
     const fullSizes = sizes.slice(breakPosition, sizes.length);
+
     const children = this.props.children;
 
     return (
       <div>
         <Hidden only={collapsedSizes}>
+          {children}
         </Hidden>
         <Hidden only={fullSizes}>
+          <Button {...this.props.buttonProps}>
+            <Icon>expand_more</Icon>
+          </Button>
           {children}
         </Hidden>
       </div>);
@@ -29,6 +36,10 @@ AutoCollapsing.propTypes = {
 
 AutoCollapsing.defaultProps = {
   breakAt: "sm",
+  buttonProps: {
+    color: "secondary",
+    varian: "raised",
+  },
 }
 
 export default AutoCollapsing;
