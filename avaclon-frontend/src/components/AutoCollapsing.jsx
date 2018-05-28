1860b7dce563004a8c75fb0c3607cd703a52de57
JoshHash <josh.hadu@gmai.com>
2018-05-28 10:57:46 +0200

    add AutoCollapsing component

---
+++ avaclon-frontend/src/components/AutoCollapsing.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+
+class AutoCollapsing extends Component {
+
+    render() {
+        return (<div className="AutoCollapsing"></div>);
+    }
+}
+
+export default AutoCollapsing;
