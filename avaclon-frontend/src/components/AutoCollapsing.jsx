ccb1f7570bb174166010a58edc0040ae24d5bf71
JoshHash <josh.hadu@gmai.com>
2018-05-28 11:13:58 +0200

    display children

--- avaclon-frontend/src/components/AutoCollapsing.jsx
+++ avaclon-frontend/src/components/AutoCollapsing.jsx
@@ -1,10 +1,34 @@
 import React, { Component } from "react";
+import PropTypes from "prop-types";
+import { Hidden } from "@material-ui/core";
+
+const sizes = ['xs', 'sm', 'md', 'lg', 'xl'];
 
 class AutoCollapsing extends Component {
 
-    render() {
-        return (<div className="AutoCollapsing"></div>);
-    }
+  render() {
+    const breakPosition = sizes.indexOf(this.props.breakAt) + 1;
+    const collapsedSizes = sizes.slice(0, breakPosition);
+    const fullSizes = sizes.slice(breakPosition, sizes.length);
+    const children = this.props.children;
+
+    return (
+      <div>
+        <Hidden only={collapsedSizes}>
+        </Hidden>
+        <Hidden only={fullSizes}>
+          {children}
+        </Hidden>
+      </div>);
+  }
+}
+
+AutoCollapsing.propTypes = {
+  breakAt: PropTypes.string.isRequired,
+}
+
+AutoCollapsing.defaultProps = {
+  breakAt: "sm",
 }
 
 export default AutoCollapsing;
