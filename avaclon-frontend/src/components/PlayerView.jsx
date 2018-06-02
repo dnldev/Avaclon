670060b32eb9e042489beeaccf0fce527c0fb498
JoshHash <josh.hadu@gmai.com>
2018-06-02 16:12:11 +0200

    break players appropriately
    a function was implemented to correctly assign the column sizes

--- avaclon-frontend/src/components/PlayerView.jsx
+++ avaclon-frontend/src/components/PlayerView.jsx
@@ -12,23 +12,28 @@ const styles = theme => ({
 });
 
 class PlayerView extends Component {
+  calculateSize(base, itemCount, baseSize, alternateSize) {
+    const overBase = itemCount % base;
+    return overBase > base / 2 ? baseSize : alternateSize;
+  }
+
   render() {
     const { classes } = this.props;
+    const { players } = this.props;
+    const playerCount = players.length;
 
     return (
-      <Grid
-        container
-        className={classes.props}
-        wrap="wrap"
-        justify="space-between"
-      >
-        {this.props.players.map(player => (
-          <Player
+      <Grid container className={classes.root} justify="center">
+        {players.map(player => (
+          <Grid
+            item
+            xs={6}
+            sm={this.calculateSize(3, playerCount, 4, 6)}
+            md={this.calculateSize(4, playerCount, 3, 4)}
             key={player.id}
-            name={player.name}
-            id={player.id}
-            role={player.role}
-          />
+          >
+            <Player name={player.name} id={player.id} role={player.role} />
+          </Grid>
         ))}
       </Grid>
     );
