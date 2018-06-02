96439ba0f8b03e2757824207986006eca6deddf4
JoshHash <josh.hadu@gmai.com>
2018-06-02 21:33:49 +0200

    make PlayerView expandable

--- avaclon-frontend/src/components/PlayerView.jsx
+++ avaclon-frontend/src/components/PlayerView.jsx
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 
 import { withStyles } from '@material-ui/core/styles';
 
+import Collapse from '@material-ui/core/Collapse';
 import Grid from '@material-ui/core/Grid';
 
 import Player from './Player';
@@ -12,10 +13,22 @@ const styles = theme => ({
 });
 
 class PlayerView extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      expanded: false,
+    };
+  }
+
   calculateSize(base, itemCount, baseSize, alternateSize) {
     const overBase = itemCount % base;
     return overBase > base / 2 ? baseSize : alternateSize;
   }
+
+  expand = () => {
+    this.setState({ expanded: true });
+  };
 
   render() {
     const { classes } = this.props;
@@ -23,19 +36,27 @@ class PlayerView extends Component {
     const playerCount = players.length;
 
     return (
-      <Grid container className={classes.root} justify="center">
-        {players.map(player => (
-          <Grid
-            item
-            xs={6}
-            sm={this.calculateSize(3, playerCount, 4, 6)}
-            md={this.calculateSize(4, playerCount, 3, 4)}
-            key={player.id}
-          >
-            <Player name={player.name} id={player.id} role={player.role} />
+      <div className={classes.root}>
+        <Collapse
+          in={this.state.expanded}
+          onClick={this.expand}
+          collapsedHeight="40px"
+        >
+          <Grid container justify="center">
+            {players.map(player => (
+              <Grid
+                item
+                xs={6}
+                sm={this.calculateSize(3, playerCount, 4, 6)}
+                md={this.calculateSize(4, playerCount, 3, 4)}
+                key={player.id}
+              >
+                <Player name={player.name} id={player.id} role={player.role} />
+              </Grid>
+            ))}
           </Grid>
-        ))}
-      </Grid>
+        </Collapse>
+      </div>
     );
   }
 }
