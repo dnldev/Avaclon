aa5f4f89b2eaab06fd9a02c6a0e712fc4eb0e47c
JoshHash <josh.hadu@gmai.com>
2018-06-04 12:12:05 +0200

    export classes from module

---
+++ shared/avalon-models/index.js
@@ -0,0 +1,9 @@
+const Player = require('./Player');
+const Role = require('./Role');
+const affiliations = require('./Affiliation');
+
+module.exports = {
+    Player: Player,
+    Role: Role,
+    Affiliation: affiliations,
+};
