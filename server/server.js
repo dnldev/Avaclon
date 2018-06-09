e5066256994e4199249b1a5890c1e1bf2fb34bd0
unknown <daniel.ungersboeck@gmail.com>
2018-06-09 11:13:19 +0200

    randomly generate id add cors

--- server/server.js
+++ server/server.js
@@ -3,8 +3,10 @@ const server = require('http').Server(app);
 const io = require('socket.io')(server);
 
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 app.use(bodyParser.json());
+app.use(cors());
 
 const express_log = require('debug')('express');
 const random_string = require('randomstring');
@@ -17,8 +19,7 @@ const lobbies = {};
 server.listen(port);
 
 app.post('/lobby', (req, res) => {
-  // let id = random_string.generate(7);
-  let id = 'a1bc23d';
+  let id = random_string.generate(7);
   let new_lobby = new Lobby(id, io.of('/lobby/' + id));
 
   lobbies[id] = new_lobby;
