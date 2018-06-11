82fdbb3a95eadf9d6c06e6ff79c1a2e60ac05ba2
unknown <daniel.ungersboeck@gmail.com>
2018-06-11 17:12:22 +0200

    update player to use playerData

--- server/classes/Player.js
+++ server/classes/Player.js
@@ -1,8 +1,10 @@
 const game_log = require('debug')('game');
 
+const PlayerData = require('../../shared/avalon-models/PlayerData');
+
 class Player {
-  constructor(name, socket) {
-    this.name = name;
+  constructor(name, roleName, socket) {
+    this.playerData = new PlayerData(socket.id, name, roleName);
     this.socket = socket;
 
     game_log('New Player: %s', this.name);
