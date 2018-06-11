f57c592f8f5d8c3c276fa3efd080361fa5fd63cb
unknown <daniel.ungersboeck@gmail.com>
2018-06-11 17:16:55 +0200

    add basic start-new-game handling with a temporary emit (to test)

--- server/classes/Lobby.js
+++ server/classes/Lobby.js
@@ -3,6 +3,13 @@ const lobby_log = require('debug')('lobby');
 const Game = require('./Game');
 const Player = require('./Player');
 
+const startingConfig = {
+  currentQuest: 0,
+  players: [],
+  voteTracker: 0,
+  wonQuests: [],
+};
+
 class Lobby {
   constructor(id, namespace) {
     this.id = id;
@@ -20,11 +27,19 @@ class Lobby {
       socket.on('new-game', data => {
         this.game = new Game(
           data.gameData,
-          new Player(data.username, socket),
+          new Player(data.username, 'Generic Blue', socket),
           this.namespace
         );
 
         lobby_log('New Game: ', this.game);
+
+        // Temporary TODO: different for each player (specific role/name)
+        this.namespace.emit('start-new-game', {
+          player: this.game.admin.playerData,
+          // players: this.game.players, // TODO: function: getObject of other Players with the current player's information
+          ...startingConfig,
+          ...this.game.gameData,
+        });
       });
 
       socket.on('join-game', name => {
