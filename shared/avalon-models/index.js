189f53acb7b2a4ef737d062e157d88124dbec3b1
JoshHash <josh.hadu@gmai.com>
2018-06-13 08:22:05 +0200

    add PlayerInformation to exports

--- shared/avalon-models/index.js
+++ shared/avalon-models/index.js
@@ -1,9 +1,11 @@
-const PlayerData = require('./PlayerData');
-const Role = require('./Role');
 const Affiliation = require('./Affiliation');
+const PlayerData = require('./PlayerData');
+const PlayerInformation = require('./PlayerInformation');
+const Role = require('./Role');
 
 module.exports = {
     Affiliation: Affiliation,
     PlayerData: PlayerData,
+    PlayerInformation: PlayerInformation,
     Role: Role,
 };
