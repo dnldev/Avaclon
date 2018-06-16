a1a4034327f48b8ba8e78832bd1c535af04fa702
unknown <daniel.ungersboeck@gmail.com>
2018-06-16 10:23:30 +0200

    change inputs to be dependant on props (so it can be reused)

--- avaclon-frontend/src/components/ConfigInput.jsx
+++ avaclon-frontend/src/components/ConfigInput.jsx
@@ -46,31 +46,35 @@ class ConfigInput extends Component {
               <Grid item xs={12} md={4}>
                 <TextField
                   className={classes.gridItemContent}
+                  autoFocus={true}
                   label={strings.name}
                   name="username"
                   onChange={context.handleChange}
+                  onKeyPress={context.onUserNameKeyPress}
                   value={context.username}
                 />
               </Grid>
-              <Grid item xs={12} md={4}>
-                <FormControl className={classes.gridItemContent}>
-                  <InputLabel htmlFor="player-count">
-                    {strings.playerCount}
-                  </InputLabel>
-                  <Select
-                    inputProps={{ name: 'playerCount', id: 'player-count' }}
-                    onChange={context.handleChange}
-                    value={context.playerCount}
-                  >
-                    <MenuItem value={5}>(5) {strings.numbers.five}</MenuItem>
-                    <MenuItem value={6}>(6) {strings.numbers.six}</MenuItem>
-                    <MenuItem value={7}>(7) {strings.numbers.seven}</MenuItem>
-                    <MenuItem value={8}>(8) {strings.numbers.eight}</MenuItem>
-                    <MenuItem value={9}>(9) {strings.numbers.nine}</MenuItem>
-                    <MenuItem value={10}>(10) {strings.numbers.ten}</MenuItem>
-                  </Select>
-                </FormControl>
-              </Grid>
+              {this.props.showPlayerCount && (
+                <Grid item xs={12} md={4}>
+                  <FormControl className={classes.gridItemContent}>
+                    <InputLabel htmlFor="player-count">
+                      {strings.playerCount}
+                    </InputLabel>
+                    <Select
+                      inputProps={{ name: 'playerCount', id: 'player-count' }}
+                      onChange={context.handleChange}
+                      value={context.playerCount}
+                    >
+                      <MenuItem value={5}>(5) {strings.numbers.five}</MenuItem>
+                      <MenuItem value={6}>(6) {strings.numbers.six}</MenuItem>
+                      <MenuItem value={7}>(7) {strings.numbers.seven}</MenuItem>
+                      <MenuItem value={8}>(8) {strings.numbers.eight}</MenuItem>
+                      <MenuItem value={9}>(9) {strings.numbers.nine}</MenuItem>
+                      <MenuItem value={10}>(10) {strings.numbers.ten}</MenuItem>
+                    </Select>
+                  </FormControl>
+                </Grid>
+              )}
             </Grid>
           );
         }}
@@ -81,6 +85,11 @@ class ConfigInput extends Component {
 
 ConfigInput.propTypes = {
   classes: PropTypes.object.isRequired,
+  showPlayerCount: PropTypes.bool,
+};
+
+ConfigInput.defaultProps = {
+  showPlayerCount: false,
 };
 
 export default withStyles(styles)(ConfigInput);
